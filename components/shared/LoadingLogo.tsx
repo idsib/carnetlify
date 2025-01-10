ed3631207f5d139ff5edfbe6a4ec54b3e2bc22e3
Jara <jjara@institutmvm.cat>
2025-01-10 20:03:34 +0100

    configurando clerk con convex para el chat, nueva base de datos que se ajusta mas a mis necesidades, pronto se adaptará (compatible con firebase)

---
+++ components/shared/LoadingLogo.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+type Props = {
+  size?: number;
+};
+
+export const LoadingLogo = ({ size = 100 }: Props) => {
+  return (
+    <div className="h-full w-full flex justify-center items-center">
+      <Image
+        src="assets/images/carnetlify.png"
+        alt="Logo"
+        width={size}
+        height={size}
+        className="animate-pulse duration-800"
+      />
+    </div>
+  );
+};

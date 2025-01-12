30375a271c155d5579deeed0b72acfab77e675ee
MajestadAyman <abourhim@institutmvm.cat>
2025-01-12 15:35:43 +0100

    Borramos los archivos de testeo en backend firebase

--- backend/firebase/updateUser.js
+++ backend/firebase/updateUser.js
@@ -1,6 +1,6 @@
 import { updateProfile, updateEmail } from "firebase/auth";
 import { auth } from '@/backend/firebase/config';
-import {updateNameUserInBackend, updateDniUserInBackend, updateAgeUserInBackend, updateCountryUserInBackend, updateProvinceUserInBackend, updateCityUserInBackend, updatePostalCodeUserInBackend, updateHomeUserInBackend, updatePhoneUserInBackend} from '@/backend/firebase/config';
+import { updateNameUserInBackend, updateDniUserInBackend, updateAgeUserInBackend, updateCountryUserInBackend, updateProvinceUserInBackend, updateCityUserInBackend, updatePostalCodeUserInBackend, updateHomeUserInBackend, updatePhoneUserInBackend } from '@/backend/firebase/config';
 
 export async function updateUserProfile(newDisplayName, newDni, newAge, newContry, newProvince, newCity, newPostalCode, newHome, newPhone, newPhotoURL, newEmail, newPassword) {
 
@@ -13,66 +13,66 @@ export async function updateUserProfile(newDisplayName, newDni, newAge, newContr
         }).catch((error) => {
             console.log("Error con actualizacion con nombre y foto: " + error)
         });
-        
+
         const userName = {
             fullName: newDisplayName
-          };
+        };
         await updateNameUserInBackend(userName)
     }
 
     if (newDni) {
         const userDni = {
             dni: newDni
-          };
+        };
         await updateDniUserInBackend(userDni)
     }
-    
+
     if (newAge) {
         const userAge = {
             age: newAge
-          };
+        };
         await updateAgeUserInBackend(userAge)
     }
 
     if (newContry) {
         const userCountry = {
             country: newContry
-          };
+        };
         await updateCountryUserInBackend(userCountry)
     }
 
     if (newProvince) {
         const userProvince = {
             province: newProvince
-          };
+        };
         await updateProvinceUserInBackend(userProvince)
     }
 
     if (newCity) {
         const userCity = {
             city: newCity
-          };
+        };
         await updateCityUserInBackend(userCity)
     }
 
     if (newPostalCode) {
         const userPostalCode = {
             postalCode: newPostalCode
-          };
+        };
         await updatePostalCodeUserInBackend(userPostalCode)
     }
 
     if (newPhone) {
         const userPhone = {
             phone: newPhone
-          };
+        };
         await updatePhoneUserInBackend(userPhone)
     }
 
     if (newHome) {
         const userHome = {
             home: newHome
-          };
+        };
         await updateHomeUserInBackend(userHome)
     }
 
@@ -99,9 +99,9 @@ export async function updateUserProfile(newDisplayName, newDni, newAge, newContr
         const user = auth.currentUser;
         updatePassword(user, newPassword).then(() => {
             console.log("Usuario actualizado con los datos => contraseña: " + newEmail)
-          }).catch((error) => {
+        }).catch((error) => {
             console.log("Error con actualización con email: " + error)
-          });
+        });
     }
-    
+
 }

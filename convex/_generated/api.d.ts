b42414158ccaf522b1dc6c6e366a6d625f5a67dc
Jara <jjara@institutmvm.cat>
2025-01-15 16:01:24 +0100

    pequeño cambio despues de un cambio de rama

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -16,8 +16,8 @@ import type {
 import type * as conversations from "../conversations.js";
 import type * as http from "../http.js";
 import type * as requests from "../requests.js";
-import type * as _utils from "../_utils.js";
 import type * as user from "../user.js";
+import type * as _utils from "../_utils.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -31,8 +31,8 @@ declare const fullApi: ApiFromModules<{
   conversations: typeof conversations;
   http: typeof http;
   requests: typeof requests;
-  _utils: typeof _utils;
   user: typeof user;
+  _utils: typeof _utils;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,

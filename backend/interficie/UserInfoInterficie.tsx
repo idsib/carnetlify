11f713d1cc526c749915f2d6e08320dc13ed5239
IDSIB <ivislump@protonmail.com>
2025-01-16 21:26:55 +0100

    now also works with personalInfo

--- backend/interficie/UserInfoInterficie.tsx
+++ backend/interficie/UserInfoInterficie.tsx
@@ -5,4 +5,12 @@ export interface UserInfo {
   plan: string;
   isLocked: string;
   profile_img: string;
+  dni?: string;
+  age?: string;
+  country?: string;
+  province?: string;
+  city?: string;
+  postalCode?: string;
+  home?: string;
+  phone?: string;
 }
